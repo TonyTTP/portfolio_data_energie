ed94914dd06cc0f9e2ce3b6a29a66d21e3e3f20d
TonyTTP <thebaudperriertony@gmail.com>
2026-09-14 10:19:51 +0200

    requete 2

--- projet-11-reporting-export-streamlit/SQL11.sql
+++ projet-11-reporting-export-streamlit/SQL11.sql
@@ -3,3 +3,20 @@ SELECT region,ROUND(AVG(CASE WHEN date >= date('now','7 days') THEN conso_mw END
 ROUND(AVG(CASE WHEN date >= date('now','14 days') AND date <= date('7 days','14 days') THEN conso_mw END),0) AS conso_semaine_prece
 FROM conso_journaliere
 GROUP BY region;
+
+--Requete 2 : Heure de pointe par saison
+
+SELECT CASE(WHEN CAST strftime('%m', date) AS INTEGRER) IN (12,1,2) THEN 'hiver'
+
+SELECT CASE(WHEN CAST strftime('%m',date) AS INTEGRER) IN (3,4,5) THEN 'printemps'
+
+SELECT CASE(WHEN CAST strftime('%m',date) AS INTEGRER) IN (6,7,8) THEN 'été'
+
+ELSE 'automne' END AS saison
+
+CAST strftime('%H', date) AS heure, ROUND(AVG(conso_mw,0)) AS conso_moy
+FROM conso_journaliere 
+
+GROUP BY region,heure
+
+ORDER BY region, heure;
