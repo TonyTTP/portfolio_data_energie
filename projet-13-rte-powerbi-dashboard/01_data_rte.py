e1f60d61f01d02db484377a66e6c0242f19f787b
TonyTTP <thebaudperriertony@gmail.com>
2026-09-18 16:04:25 +0200

    ajout colonnes et renommage pour pbi

--- projet-13-rte-powerbi-dashboard/01_data_rte.py
+++ projet-13-rte-powerbi-dashboard/01_data_rte.py
@@ -72,6 +72,54 @@ def recup_eco2mix(debut_date="2024-01-01",fin_date="2024-10-01"):
         print("Accès impossible à Eco2mix")
 
 
+df_rte = recup_eco2mix()
 
 
+connect = sqlite3.connect("data_reelle")
+df_rte.to_sql(
+    "eco_mix",
+    connect,
+    if_exists="replace",
+    index=False
+)
+
+df_rte.to_csv(
+    "data_eco2mix.csv",
+    index=False
+)
+
+df = pd.read_csv("data_eco2mix.csv")
+
+print(f"Colonnes : {df.columns.tolist()} ")
+print(f"Nombre de valeurs manquantes : {df_rte.isnull().sum()}")
+
+
+df = df.rename(columns={
+    "date_heure" : "datetime",
+    "consommation_brute_total" : "conso_tot_mw",
+    "consommation_brute_gaz" : "conso_gaz_mw",
+    "consommation_brute_fioul" : "conso_fioul_mw"
+})
+
+df["datetime"] = pd.to_datetime(df["datetime"])
+df["date"] = df["datetime"].dt.date
+df["heure"] = df["datetime"].dt.hour
+df["mois"] = df["datetime"].dt.month
+df["jour_semaine"] = df["datetime"].dt.dayofweek
+
+def saison(mois):
+    if mois in [12,1,2]:
+        return "hiver"
+    elif mois in [3,4,5]: 
+        return "printemps"
+    elif mois in [6,7,8]: 
+        return "été"
+    else:
+        return "automne"
+
+df["saison"] = df["mois"].apply(saison)
+
+
+df.to_csv("rte_powerbi_ready",index=False,encoding="utf-8-sig")
+print(df.head)
 
